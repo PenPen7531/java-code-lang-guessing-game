f21bffa8ed6ad68c01f7204c9c6888792b234e70
Jeff7531 <jeffmkwang@gmail.com>
2022-08-04 03:01:38 -0700

    Changed game to name guessing to coding language guessing game.

--- src/App.java
+++ src/App.java
@@ -21,26 +21,15 @@ public class App {
 
     public String getRandomName(){
         String[] names={ 
-        "jeff", 
-        "wendy", 
-        "jack",
-        "leah", 
-        "thomas", 
-        "cindy", 
-        "chelsey", 
-        "carlos", 
-        "abby", 
-        "christopher", 
-        "xavier", 
-        "becky", 
-        "diego", 
-        "gabriel", 
-        "harry", 
-        "indigo", 
-        "kelly", 
-        "sally", 
-        "trisha", 
-        "victoria"};
+        "c plus plus", 
+        "css", 
+        "html",
+        "python", 
+        "javascript", 
+        "c sharp",
+        "carbon",
+        "c",
+        "mysql"};
         Random r = new Random();
         int random_index=r.nextInt(names.length);
         String random_name=names[random_index];
@@ -51,7 +40,7 @@ public class App {
     public boolean check_answer(String guess, String answer){
         guess=guess.strip();
         answer=answer.strip();        
-        if (guess.equals(answer)){
+        if (guess.toLowerCase().equals(answer.toLowerCase())){
             return true;
         }
         else {
@@ -63,16 +52,22 @@ public class App {
         
         boolean reply=true;
      
-        System.out.println("Hello Welcome to the name guessing game\nWould you like to see the rules?");
+        System.out.println("Hello Welcome to the coding language guessing game\nWould you like to see the rules?");
         while (reply){
             String answer=scan.nextLine();
             answer.strip();
             if (answer.equals("yes")){
-                System.out.println("A blank hidden name will be given, you must guess the correct name within 10 tries.");
+                System.out.println("--------------------------");
+                System.out.println("---------- RULES ---------");
+                System.out.println("A blank language will be displayed.\nYou must guess the correct name within 10 tries.\n3 hints will be given after the first 3 guesses");
+                System.out.println("--------------------------");
                 reply=false;
             }
             else if (answer.equals("no")){
-                System.out.println("Lets begin the game!");
+                
+                System.out.println("--------------------------");
+                System.out.println("----- BEGINNINER GAME ----");
+                System.out.println("--------------------------");
                 reply=false;
             }
             else{
@@ -86,14 +81,19 @@ public class App {
     public boolean play_again(){
         boolean play_a=true;
         while (play_a){
-            System.out.println("Would you like to play agian");
+        System.out.println("Would you like to play again.\nyes/no:");
         String answer=scan.nextLine();
         if (answer.equals("yes")){
-            System.out.println("Resetting Game");
+            System.out.println("--------------------------");
+            System.out.println("----- RESETTING GAME -----");
+            System.out.println("--------------------------");
             return true;
         }
         else if (answer.equals("no")){
-            System.out.println("Thank you for playing. GAME OVER");
+            System.out.println("Thank you for playing.");
+            System.out.println("--------------------------");
+            System.out.println("------- GAME OVER --------");
+            System.out.println("--------------------------");
             return false;
         }
         else{
@@ -116,21 +116,48 @@ public class App {
             String random_name=game.getRandomName();
             System.out.println(random_name);
             String blank=game.convertBlank(random_name);
-            System.out.println(blank);
+            
             String guess_name;
+            int guesses=10;
             while (round){
-                
+                System.out.println(blank);
                 System.out.println("Guess the name: ");
                 guess_name=scan.nextLine();
-                System.out.println("Your guess was "+guess_name);
+                System.out.println("Your guess was "+guess_name.toUpperCase());
+                guesses--;
                 boolean result=game.check_answer(guess_name, random_name);
-                if (result){
-                    System.out.println("Winner");
+                if (result & guesses>0){
+                    System.out.println("--------------------------");
+                    System.out.println("--------- WINNER ---------");
+                    System.out.println("--------------------------");
                     round=false;
                     play=game.play_again();
+                    
+                }
+                else if (guesses == 0 & !(result)){
+                    System.out.println("Ran out of guesses");
+                    System.out.println("--------------------------");
+                    System.out.println("------- ROUND OVER -------");
+                    System.out.println("--------------------------");
+                    play=game.play_again();
+                    round=false;
                 }
                 else{
+
                     System.out.println("Good guess but not correct. Try again");
+                    System.out.println("--------------------------");
+                    System.out.println("----------- HINTS --------");
+                    System.out.println("--------------------------");
+                    if (guesses < 10){
+                        System.out.println("HINT 1: The length of the name is " + random_name.length());
+                    }
+                    if (guesses < 9){
+                        System.out.println("HINT 2: The first character of the name is " + random_name.charAt(0));
+                    }
+                    if (guesses < 8){
+                        System.out.println("HINT 3: The last character of the name is " + random_name.charAt(random_name.length() - 1));
+                    }
+                    System.out.println("Guesses Left: " + guesses);
                 }
 
             }
